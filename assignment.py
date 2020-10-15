1ca8e68051535758f667a158201a626fa2462882
MayaDesrochers <mayad459@deltalearns.ca>
2020-10-15 11:09:26 -0700

    update

--- assignment.py
+++ assignment.py
@@ -16,9 +16,8 @@ def instructions():
     # Will display instructions
     # input parameters: none needed
     # output parameters: None
-    # Author:
+    # Author: Maya 
     # Modified:
-    return None
     print("==============")
     print("Instructions:")
     print("-This program is able to calculate the volume of a cube, cone, pyramid, or rectangle.")
@@ -26,15 +25,17 @@ def instructions():
     print("-The user will then be asked to enter the dimensions of the shape.")
     print("-The program will then calculate the volume of the shape entered by the user.")
     print("-Once the volume has been calculated, the program will repeat unless the user chooses to quit the program.")
-    print('-To quit the program, enter "quit".')
+    print('-To quit the program, enter "quit" when prompeted.')
     print("Example:")
     print("Enter a shape:'rectangle'")
     print("Enter length:'3'")
     print("Enter width:'4'")
     print("Enter height:'5'")
     print("The volume of the rectangle you entered is 60.")
+    print("Quit program?:'quit'")
     print("==============")
-instructions()
+    return None
+
 def getParams(shape):
     # Will create a list of questions to be asked depending on the shape.
     # These will be asked so that the user can enter in appropriate values
